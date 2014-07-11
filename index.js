473763e0cff8874f57f6bfe15d353ef158245240
remixz <zbruggeman@me.com>
2014-07-11 00:29:26 -0700

    Mobile improvements

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var sprintf = require('util').format;
 var emoji = require('./emoji/emoji.js');
-emoji.init_env();
+emoji.allow_native = false;
+
+var fastclick = require('fastclick');
+fastclick(document.body);
 
 var parts = {
     head: ['dog', 'cat', 'mouse', 'hamster', 'rabbit', 'tiger', 'frog', 'koala', 'bear', 'pig', 'cow', 'monkey_face', 'panda_face', 'japanese_ogre', 'skull', 'alien', 'hankey', 'jack_o_lantern', 'santa', 'sun_with_face', 'full_moon_with_face', 'smiley_cat', 'smile_cat', 'heart_eyes_cat', 'kissing_cat', 'smirk_cat', 'scream_cat', 'crying_cat_face', 'joy_cat', 'pouting_cat', 'man_with_gua_pi_mao', 'man_with_turban', 'cop', 'construction_worker', 'guardsman', 'baby', 'boy', 'girl', 'man', 'woman', 'older_man', 'older_woman', 'person_with_blond_hair', 'angel', 'princess'],
@@ -10,11 +13,10 @@ var parts = {
     left_arm: ['iphone', 'wine_glass', 'cocktail', 'handbag', 'briefcase', 'icecream', 'cookie', 'basketball', 'moneybag', 'video_game', 'balloon', 'movie_camera', 'loudspeaker', 'mega', 'gun', 'smoking', 'microphone', 'saxophone', 'trumpet', 'fishing_pole_and_fish', 'tropical_drink', 'lollipop', 'ok_hand', 'wave', 'fist', 'facepunch', 'hand', 'point_left', 'muscle', 'chocolate_bar', 'spaghetti', 'banana', 'oden', 'dango', 'egg', 'hammer', 'baby_bottle'],
     right_arm: ['iphone', 'wine_glass', 'cocktail', 'handbag', 'briefcase', 'icecream', 'cookie', 'basketball', 'moneybag', 'video_game', 'telephone_receiver', 'tada', 'hocho', 'syringe', 'violin', 'guitar', 'closed_umbrella', '+1', '-1', 'point_right', 'pizza', 'poultry_leg', 'meat_on_bone', 'notebook', 'ledger', 'notebook_with_decorative_cover', 'postal_horn', 'art', 'clapper', 'football', 'rugby_football']
 }
-var template = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s<br>' + (emoji.replace_mode === 'unified' ? '&nbsp;&nbsp;' : '') + '%s%s%s<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s%s<br>'
+var template = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s<br>%s%s%s<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s%s<br>'
 var tweetTemplate = '    %s \n    %s \n  %s%s%s\n   %s%s\n';
 
 var holder = document.querySelector('.holder');
-if (emoji.replace_mode === 'unified') holder.className += ' native';
 
 var tweetBtn = document.querySelector('.tweet');
 var generate = function (username) {
